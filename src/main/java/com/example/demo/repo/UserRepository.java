5701ae0e415824a9ba54c2558a01fd84834775cd
Atynov Arystan <77671290+Arystan97@users.noreply.github.com>
2021-09-29 02:05:13 -0700

    Delete UserRepository.java

--- src/main/java/com/example/demo/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
